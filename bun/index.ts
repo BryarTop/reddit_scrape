fc6f2173ab57232f9e9f422213f818adea131b04
Bryar Topham <BryTop05@gmail.com>
2023-12-11 17:46:34 -0700

    initial commit

---
+++ bun/index.ts
@@ -0,0 +1,37 @@
+import puppeteer from "puppeteer";
+
+const scrapeInfiniteScroll = async (url:string, numScrolls:number):Promise<string[]> => {
+	const browser = await puppeteer.launch({
+		headless:"new",
+		// executablePath:'/Applications/Google Chrome.app'
+	});
+	const page = await browser.newPage()
+
+	await page.goto(url);
+	
+	async function scrollDown(){
+		await page.evaluate(() => {
+			window.scrollto(0,document.body.scrollHeight);
+		});
+		await page.waitForTimeout(1000);
+	}
+
+	for (let i:number = 0; i<numScrolls; i++){
+		await scrollDown();
+	};
+	
+	const scrapedData:string[] = await page.evaluate(()=> {
+		const items = document.querySelectorAll('div[id^="t3_"]:not([id$="share-menu"]):not([id*="="])');
+
+		const data:string[] = [];
+		items.forEach(item => {
+			data.push(item.innerText);
+		});
+		return data;
+	})
+
+	await browser.close();
+	return scrapedData;
+};
+
+scrapeInfiniteScroll('https://www.reddit.com/r/WorkReform/',5);
