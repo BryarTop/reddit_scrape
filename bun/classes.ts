223931f386cdbd8e13dd94ff763cbf43888d6d54
Bryar Topham <BryTop05@gmail.com>
2023-12-12 16:43:10 -0700

    threw some classes together to return JSON

---
+++ bun/classes.ts
@@ -0,0 +1,10 @@
+export class RedditObject {
+	public url: string;
+	public innerText: string;
+
+	constructor(url:string, innerText:string) {
+		this.url = url;
+		this.innerText = innerText;
+	};
+};
+
--- bun/index.ts
+++ bun/index.ts
@@ -1,7 +1,17 @@
 import puppeteer from "puppeteer";
 import fs from 'fs';
+// import { RedditObject } from './classes';
 
-const scrapeInfiniteScroll = async (url:string, numScrolls:number):Promise<object[]> => {
+class RedditObject {
+	url:string;
+	innerText:string;
+	constructor(url:string, innerText:string){
+		this.url = url;
+		this.innerText = innerText;
+	}
+}
+
+const scrapeInfiniteScroll = async (url:string, numScrolls:number):Promise<RedditObject[]> => {
 	const browser = await puppeteer.launch({
 		headless:"new",
 		// executablePath:'/Applications/Google Chrome.app'
@@ -9,7 +19,7 @@ const scrapeInfiniteScroll = async (url:string, numScrolls:number):Promise<objec
 	const page = await browser.newPage()
 
 	await page.goto(`https://www.reddit.com/r/${url}/top/?t=year`);
-
+	
 	async function scrollDown(){
 		await page.evaluate(() => {
 			window.scrollTo(0,document.body.scrollHeight);
@@ -20,27 +30,29 @@ const scrapeInfiniteScroll = async (url:string, numScrolls:number):Promise<objec
 	for (let i:number = 0; i<numScrolls; i++){
 		await scrollDown();
 	};
-	
-	const scrapedData:object[] = await page.evaluate(()=> {
-		const items = document.querySelectorAll('div[id^="t3_"]:not([id$="share-menu"]):not([id*="="])');
+	try {
+		const scrapedData:RedditObject[] = await page.evaluate(()=> {
+			const items = document.querySelectorAll('div[id^="t3_"]:not([id$="share-menu"]):not([id*="="])');
 
-		const data:object[] = [];
-		items.forEach(item => {
-			data.push(
-				{
-					'subreddit':url,
-					'postTitle':item.innerText
-				}
-			);
-		});
-		return data;
-	})
+			const data:RedditObject[] = [];
+			items.forEach((item) => {
+				const itemObj = new RedditObject("","");
+				itemObj.url = url;
+				itemObj.innerText = item.innerText
+				data.push(itemObj);
+			});
+			return data;
+		})}
+	catch (err){
+		await browser.close();
+		console.error(err);
+	}
 
 	await browser.close();
 	return scrapedData;
 };
 
-const writeDataToFile = async (data:object[]) => {
+const writeDataToFile = async (data:RedditObject[]) => {
 	 fs.appendFile('./output.txt', data.join('\n'),"utf-8",(err)=>{
 										 if(err){
 												console.error(err);
@@ -54,7 +66,7 @@ const writeDataToFile = async (data:object[]) => {
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
 
-const subreddits:string[] = ['WorkReform','jobs','antiwork','wfh','managers'];
+const subreddits:string[] = ['WorkReform'];/* ,'jobs','antiwork','wfh','managers']; */
 
 for(let subR of subreddits){
 	try {
